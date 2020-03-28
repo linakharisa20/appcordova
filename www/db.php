69a5ee5fb4665fcf1777f9bfaa0ad3a82a9fc31c
13AhmadArtyantoSaputra <ahmadartyantosaputra@gmail.com>
2020-03-29 00:08:13 +0700

    aplikasi cordova

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id12789058_linaharitsah","lina260898","id12789058_lina") or die ("could not connect database");
+?>
